c41e3d719a8e9fa62c8fd7ee0e71c2d605c85fe8
ThorKild_ <115811899+MasiaAntoine@users.noreply.github.com>
2022-11-19 00:39:06 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -15,6 +15,12 @@
         header("Location: connexion.php");
         die();
     }
+
+    //IMPORTANT : si tu souhaite te déconnecté tu devras utiliser
+    //la fonction session_destroy() à toi d'adapter dans ton code.
+    //ça permet comme son nom l'indite de détruire la session.
+    //Après au bout d'un certain temps ton serveur devrais la supprimer.
+    //Mais je te conseil de toujours avoir un petit bouton de déconnexion.
 ?>
 
 <!DOCTYPE html>
